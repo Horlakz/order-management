8d1c6a7b9b8d9de194bba7128051e338953aecea
Olakotan Daniel <officialhorlakz@gmail.com>
2025-01-01 13:30:34 +0100

    feat(chat and order controllers): add chat and order management functionalities with pagination support

---
+++ src/modules/order/controllers/chat.controller.ts
@@ -0,0 +1,64 @@
+import { User } from '@/lib/decorators/user';
+import { PageableDto } from '@/lib/dto/dto';
+import { BasePaginatedResponse, BaseResponse } from '@/lib/payload/response';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ChatService } from '../services/chat.service';
+
+@Controller('chat')
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Get(':chatroomId')
+  async getChatsByChatRoomId(
+    @User('id') userId: string,
+    @Param('chatroomId', ParseUUIDPipe) chatRoomId: string,
+    @Query() pageable: PageableDto,
+  ) {
+    const res = await this.chatService.getChatsByChatRoomId(
+      userId,
+      chatRoomId,
+      pageable,
+    );
+
+    return new BasePaginatedResponse(
+      HttpStatus.OK,
+      'Chats fetched successfully',
+      res.pagination,
+      res.data,
+    );
+  }
+
+  @Post(':chatroomId')
+  async sendMessage(
+    @User('id') userId: string,
+    @Param('chatroomId', ParseUUIDPipe) chatRoomId: string,
+    @Body('message') message: string,
+  ) {
+    return new BaseResponse(
+      HttpStatus.CREATED,
+      'Message sent successfully',
+      await this.chatService.createChatMessage(userId, chatRoomId, message),
+    );
+  }
+
+  @Post(':chatroomId')
+  async joinChatRoom(
+    @User('id') userId: string,
+    @Param('chatroomId', ParseUUIDPipe) chatRoomId: string,
+  ) {
+    return new BaseResponse(
+      HttpStatus.OK,
+      'Chatroom joined successfully',
+      await this.chatService.joinChatRoom(userId, chatRoomId),
+    );
+  }
+}
---
+++ src/modules/order/controllers/order.controller.ts
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+import { IsRole } from '@/lib/decorators/role';
+import { User } from '@/lib/decorators/user';
+import { PageableDto } from '@/lib/dto/dto';
+import { BasePaginatedResponse, BaseResponse } from '@/lib/payload/response';
+import { OrderCreateDto, OrderPageableDto } from '../order.dto';
+import { OrderService } from '../services/order.service';
+
+@Controller('order')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @IsRole('USER')
+  @Post()
+  async createOrder(@User('id') userId: string, @Body() body: OrderCreateDto) {
+    return new BaseResponse(
+      HttpStatus.OK,
+      'Order created successfully',
+      await this.orderService.createOrder(userId, body),
+    );
+  }
+
+  @IsRole('ADMIN')
+  @Get('all')
+  async getAllOrders(@Query() query: OrderPageableDto) {
+    const res = await this.orderService.getAllOrders(query);
+    return new BasePaginatedResponse(
+      HttpStatus.OK,
+      'Orders fetched successfully',
+      res.pagination,
+      res.data,
+    );
+  }
+
+  @IsRole('USER')
+  @Get()
+  async getUserOrders(@User('id') userId: string, @Query() query: PageableDto) {
+    const res = await this.orderService.getAllOrders({ userId, ...query });
+    return new BasePaginatedResponse(
+      HttpStatus.OK,
+      'Orders fetched successfully',
+      res.pagination,
+      res.data,
+    );
+  }
+
+  @IsRole('ADMIN')
+  @Post(':id/process')
+  async processOrder(
+    @User('id') userId: string,
+    @Param('id', ParseUUIDPipe) orderId: string,
+    @Body('summary') summary: string,
+  ) {
+    return new BaseResponse(
+      HttpStatus.OK,
+      'Order processed successfully',
+      await this.orderService.processOrder(userId, { orderId, summary }),
+    );
+  }
+
+  @IsRole('ADMIN')
+  @Post(':id/complete')
+  async completeOrder(
+    @User('id') userId: string,
+    @Param('id', ParseUUIDPipe) orderId: string,
+  ) {
+    return new BaseResponse(
+      HttpStatus.OK,
+      'Order completed successfully',
+      await this.orderService.completeOrder(userId, orderId),
+    );
+  }
+}
